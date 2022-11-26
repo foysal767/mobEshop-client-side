cd4d37ee3a9d39a10f3508fba2c231c56b2ee3fe
foysal767 <uddinmohammadfoysal981@gmail.com>
2022-11-26 19:56:39 +0600

    all sellers loaded

--- src/Pages/Dashoboard/AllSellers/AllSellers.js
+++ src/Pages/Dashoboard/AllSellers/AllSellers.js
@@ -1,9 +1,61 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Loading from '../../Shared/Loading/Loading';
 
 const AllSellers = () => {
+    const { data: allsellers = [], isLoading } = useQuery({
+        queryKey: ['allsellers'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/allsellers')
+            const data = await res.json()
+            return data
+        }
+    })
+    if (isLoading) {
+        return <Loading></Loading>
+    }
     return (
         <div>
-            <h2>This is all sellers page</h2>
+            <h1 className='text-3xl font-semibold my-8'>Total Buyers: {allsellers.length}</h1>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            allsellers?.map((seller, i) => <tr key={seller._id}>
+                                <th>{i + 1}</th>
+                                <td>{seller.name}</td>
+                                <td>{seller.email}</td>
+                                <td>
+                                    {
+                                        seller?.status === 'verified' ?
+                                        <button className='btn btn-accent' disabled>Verified</button>
+                                        :
+                                        <Link><button className='btn btn-secondary text-white'>Verify</button></Link>
+                                    }
+                                </td>
+                                <td>
+                                    {
+                                        seller &&
+                                        <Link>
+                                            <button className='btn btn-error text-white'>Delete</button>
+                                        </Link>
+                                    }
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
