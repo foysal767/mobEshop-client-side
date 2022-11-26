950b9888453054e215f2549e1727dd0e3cfda40c
foysal767 <uddinmohammadfoysal981@gmail.com>
2022-11-26 19:47:32 +0600

    all buyers loaded

--- src/Pages/Dashoboard/MyProducts/AllBuyers/AllBuyers.js
+++ src/Pages/Dashoboard/MyProducts/AllBuyers/AllBuyers.js
@@ -1,9 +1,52 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Loading from '../../../Shared/Loading/Loading';
 
 const AllBuyers = () => {
+    const { data: allbuyers = [], isLoading } = useQuery({
+        queryKey: ['allbuyers'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/allbuyers')
+            const data = await res.json()
+            return data
+        }
+    })
+    if (isLoading) {
+        return <Loading></Loading>
+    }
     return (
         <div>
-            <h1>This is all buyers page</h1>
+            <h1 className='text-3xl font-semibold my-8'>Total Buyers: {allbuyers.length}</h1>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            allbuyers?.map((buyer, i) => <tr key={buyer._id}>
+                                <th>{i + 1}</th>
+                                <td>{buyer.name}</td>
+                                <td>{buyer.email}</td>
+                                <td>
+                                    {
+                                        buyer &&
+                                        <Link>
+                                            <button className='btn btn-error text-white'>Delete</button>
+                                        </Link>
+                                    }
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
